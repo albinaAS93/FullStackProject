fbf66cfc547c09abe144002760eec7d6f9a73b25
albinaAS93 <102250096+albinaAS93@users.noreply.github.com>
2022-09-21 00:19:02 +0200

    form

--- fe-angular/src/app/components/login/login.component.ts
+++ fe-angular/src/app/components/login/login.component.ts
@@ -14,11 +14,7 @@ export class LoginComponent implements OnInit {
     private formBuilder: FormBuilder,
     private formService: FormService,
     private router: Router
-<<<<<<< HEAD
   ) {
-=======
-  ) { 
->>>>>>> f5d6adb5b7e2bb9b1a506664f1fd30b2f87d28b9
     this.loginForm();
   }
 
@@ -40,46 +36,24 @@ export class LoginComponent implements OnInit {
 
   get f() {
     return this.requiredForm.controls;
-<<<<<<< HEAD
-=======
-  }
-
-  loginForm(){
-    this.requiredForm = this.formBuilder.group({
-      username: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required] ]
-    })
->>>>>>> f5d6adb5b7e2bb9b1a506664f1fd30b2f87d28b9
   }
 
   submit() {
 
     this.submitted = true;
-<<<<<<< HEAD
 
     if(this.requiredForm.invalid){
       alert('Tutti i campi sono obbligatori');
       return;
     }
-=======
->>>>>>> f5d6adb5b7e2bb9b1a506664f1fd30b2f87d28b9
 
     localStorage.setItem('isLogged', '0');
 
     console.log(this.requiredForm.value);
 
-<<<<<<< HEAD
-=======
-    if(this.requiredForm.invalid){
-      alert('Tutti i campi sono obbligatori');
-      return;
-    }
->>>>>>> f5d6adb5b7e2bb9b1a506664f1fd30b2f87d28b9
     this.formService.login(JSON.stringify(this.requiredForm.value)).subscribe(res => {
       this.data = res;
 
-      this.router.navigate(['/']);
-      
       console.log(this.data.message);
 
       if(this.data.message == '1') {
@@ -88,7 +62,6 @@ export class LoginComponent implements OnInit {
       }else {
         console.log("Email o password errata");
       }
-
 
     });
 
